d629185578a9be1cfb6d1b5d106aabb67ad07741
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2024-03-13 15:59:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,37 @@ if [! -f "${PREFIX}/bin/wget" ];then
 	INSTALL_WGET="wget"
 	PKG="true"
 fi
+if [! -f "${PREFIX}/bin/perl" ];then
+	INSTALL_PERL="perl"
+	PKG="true"
+fi
 if [ "${PKG}" == "true" ];then
 	echo "Installing missing packages..."
-	pkg install "${INSTALL_PROOT}" "${INSTALL_WGET}"
+	pkg install "${INSTALL_PROOT}" "${INSTALL_WGET}" "${INSTALL_PERL}"
 fi
 echo "Installing ubuntu proot..."
 proot-distro install ubuntu || exit 2
 UBUNTU_DIR="${PREFIX}/var/lib/proot-distro/installed-rootfs/ubuntu"
 cd "${UBUNTU_DIR}/usr/bin"
-wget -q --show-progress https://raw.githubusercontent.com/pi-dev500/termuxubuntu-x11/raw/main/x11-splash.py
+wget -q https://raw.githubusercontent.com/pi-dev500/termuxubuntu-x11/raw/main/x11-splash.py -O x11-splash.py
+chmod +x x11-splash.py
+clear
+echo "Input the credentials you want:"
+echo -n "USERNAME: "
+read username
+echo -n "PASSWORD: "
+read -s password
+echo -n "CONFIRM PASSWORD: "
+read -s cpassword
+if [ ! "${password}" == "${cpassword}" ];then
+	echo "Passwords don't match, please retry: "
+	echo -n "PASSWORD: "
+	read -s password
+	echo -n "CONFIRM PASSWORD: "
+	read -s cpassword
+	if [ ! "${password}" == "${cpassword}" ];then
+		echo "Passwords don't match, exiting..."
+		exit 203
+	fi
+fi
+CPASS=`perl -e 'print crypt("${password}", "salt"),"\n"'`
