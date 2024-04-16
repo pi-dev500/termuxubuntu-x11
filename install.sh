0d05a385c48358d141e15d388ee8a1635e1db29f
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2024-04-16 10:54:39 +0200

    Update install.sh
    better launch performance, pulseaudio support, and working (lighter) splashscreen

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ if [! -f "${PREFIX}/bin/termux-x11" ];then
 fi
 if [ "${PKG}" == "true" ];then
 	echo "Installing missing packages..."
-	pkg install "${INSTALL_PROOT}" "${INSTALL_WGET}" "${INSTALL_PERL}" "${INSTALL_X11}"
+	pkg install "${INSTALL_PROOT}" "${INSTALL_WGET}" "${INSTALL_PERL}" "${INSTALL_X11}" pulseaudio
 fi
 echo "Installing ubuntu proot..."
 proot-distro install ubuntu || exit 2
@@ -51,18 +51,22 @@ fi
 # crypt password to make it understandable by adduser
 CPASS=`perl -e 'print crypt("${password}", "salt"),"\n"'`
 echo "Configurating proot..."
-echo -e "apt update && apt install fakeroot sudo xfce4 firefox \n useradd -m -p '${CPASS}' ${username}" | proot-distro login ubuntu
+echo -e "apt update
+apt install fakeroot sudo xfce4 firefox
+pip install customtkinter --break-system-packages
+useradd -m -p '${CPASS}' ${username}" | proot-distro login ubuntu
 echo "
 #!/data/data/com.termux/files/usr/bin/bash
 TERMUX_PREFIX=\${PREFIX}
+
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null &
 if [ -f \"\${TERMUX_PREFIX}/tmp/isDE\" ];then
   echo \"Desktop already started, exiting now...\"
-  am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null
   exit 0
 fi
 echo ubuntu> \"\${TERMUX_PREFIX}/tmp/isDE\"
 echo \"Starting Ubuntu xfce desktop...\"
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null &
+pulseaudio --start
 termux-x11 :1 &
 X11_PID=\$!
 echo -e \"export DISPLAY=:1\nxfwm4&\nx11-splash.py & \nsudo service dbus start\nxfce4-session \" | proot-distro login --user \"${username}\" ubuntu --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp --fix-low-ports &>/dev/null
